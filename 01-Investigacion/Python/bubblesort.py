2560deae6a39ed2d5dc070ad4ea936a9d39140eb
Ariel Rotolo <109922367+Ariel-Rotolo@users.noreply.github.com>
2023-08-24 21:57:18 -0300

    Update bubblesort.py

--- 01-Investigacion/Python/bubblesort.py
+++ 01-Investigacion/Python/bubblesort.py
@@ -1,15 +1,18 @@
-from time import time
+import time
+import random
 
-class BubbleSort:
-    @staticmethod
-    def sort(array):
-        start_time = time()
+# Bubble sort en Python
+def bubbleSort(array):
+    len_arr = len(array) # Guardo la longitud del array
+    for i in range(len_arr): # Primer loop que recorre todo el array
+        for j in range(0, len_arr - i - 1): # Segundo loop comparando los elementos adyacentes entre si
+            if array[j] > array[j + 1]: # Cambiar > a < para hacer orden descendente
+                arr[j], arr[j+1] = arr[j+1], arr[j] # Hace un cambio entre los elementos del array
 
-        len_arr = len(array) # Guardo la longitud del array
-        for i in range(len_arr): # Primer loop que recorre todo el array
-            for j in range(0, len_arr - i - 1): # Segundo loop comparando los elementos adyacentes entre si
-                if array[j] > array[j + 1]: # Cambiar > a < para hacer orden descendente
-                    array[j], array[j+1] = array[j+1], array[j] # Hace un cambio entre los elementos del array
-
-        end_time = time()
-        return end_time - start_time
+if __name__ == "__main__":
+    arr = [random.randint(1,100000) for _ in range(10000)]
+    start_time = time.time()
+    bubbleSort(arr)
+    end_time = time.time()
+    print(arr)
+    print("Tiempo de ejecución en segundos:", end_time - start_time)
